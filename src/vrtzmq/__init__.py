21247ea33f1964c53ff3f0aa69e46eb96641a79b
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2023-03-11 21:19:40 +0100

    Add is_live

--- src/vrtzmq/__init__.py
+++ src/vrtzmq/__init__.py
@@ -5,6 +5,9 @@ import struct
 from datetime import datetime
 
 
+__all__ = ["VRTSubscriber"]
+
+
 class VRTSubscriber:
     def __init__(self, host="127.0.0.1", port=50100, hwm=10000, timeout=1):
         context = zmq.Context()
@@ -19,7 +22,10 @@ class VRTSubscriber:
     def get_context_packet(self):
         """Get the first context package from the stream (discard data packets)"""
         while True:
-            packet = self.zmq_subscriber.recv()
+            try:
+                packet = self.zmq_subscriber.recv()
+            except zmq.Again as e:
+                raise RuntimeError("Timed out connecting to VRT stream") from e
             if packet[0] == 73:
                 return packet
 
@@ -30,6 +36,15 @@ class VRTSubscriber:
         context_packet = self.get_context_packet()
         (unixtime_seconds,) = struct.unpack("!I", context_packet[16:20])
         return datetime.utcfromtimestamp(unixtime_seconds)
+
+    def is_live(self, tolerance_s=2):
+        """
+        Returns True if the VRT stream is running now; False if the timestamp
+        in the stream does not match the current system clock.
+        """
+        stream_time = self.get_time()
+        now = datetime.now()
+        return abs((now - stream_time).seconds) < tolerance_s + 0.1
 
     def get_frequency(self):
         """Get the center frequency from the stream"""
