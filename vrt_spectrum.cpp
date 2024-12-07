8ddd1cc94ab9ac9de7c82c596e06cbade3e1031d
Thomas Telkamp <tf@telkamp.eu>
2024-12-07 16:22:12 +0100

    add time to gnuplot title

--- vrt_spectrum.cpp
+++ vrt_spectrum.cpp
@@ -11,6 +11,9 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/thread/thread.hpp>
 
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/posix_time/posix_time_io.hpp>
+
 #include <chrono>
 // #include <complex>
 #include <csignal>
@@ -696,7 +699,8 @@ int main(int argc, char* argv[])
                             if (has_source) {
                                 printf("set title \"%s\" font \",14\"\n;", source.c_str());
                             } else if (tracker_ext_context.tracker_ext_context_received) {
-                                printf("set title \"%s\" font \",14\"\n;", tracker_ext_context.object_name);
+                                printf("set title \"%s (%s)\" font \",14\"\n;", tracker_ext_context.object_name, 
+                                    boost::posix_time::to_iso_extended_string(boost::posix_time::from_time_t(seconds)).c_str());
                             }
                                 
                             if (vrt_context.sample_rate <= 100e3)
